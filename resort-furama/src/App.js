750ed9d7356cb62d7cb651792748ca28738aeb62
thanhhaiqb234 <thanhhaiqb234@gimal.com>
2023-12-20 08:51:23 +0700

    nop case

--- resort-furama/src/App.js
+++ resort-furama/src/App.js
@@ -4,12 +4,16 @@ import Service from "./component/ServiceManager";
 import Footer from "./component/Footer";
 import Header from "./component/Header";
 import Navigation from "./component/Navigation";
+import ServiceEdit from "./component/ServiceEdit";
+import ServiceCreate from "./component/ServiceCreate";
 function App() {
   return (
     <>
         <Header/>
         <Navigation/>
       <Footer/>
+        <ServiceEdit/>
+        <ServiceCreate/>
     </>
   );
 }
